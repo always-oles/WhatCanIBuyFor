b8710ea32798d402ba511d3c060b44ed29bd0d67
Oles <bolotniuk.oles@gmail.com>
2017-12-04 00:18:08 +0200

    angular ejected, main form done

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { MainFormComponent } from './main-form/main-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainFormComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
